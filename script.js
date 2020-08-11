132ea5dbf859988910fd608f05ecf0003c8bc5b5
gaizenband <gazik211@gmail.com>
2020-08-11 13:52:04 +0300

    1st commit

---
+++ script.js
@@ -0,0 +1,29 @@
+var init = () => {
+    //1.Tempreature
+
+    var celcius = 12;
+    var farenheit = (9/5)*celcius+32;
+    alert('12 градусов по цельсию = '+farenheit+' градусов по фаренгейту');
+
+    //2.Variables
+    var admin = 'Василий';
+    var name;
+    name = admin;
+    alert(name);
+
+    //3.Concatenation
+    alert('1000 + "108" = 1000108');
+
+    //4.Variables_additional_homework
+    var a = 10;
+    var b = 20;
+    a += b;
+    b = a - b;
+    a -= b;
+
+    alert(a+' & '+b);
+    //'123'
+}
+
+
+document.addEventListener('DOMContentLoaded',init);
